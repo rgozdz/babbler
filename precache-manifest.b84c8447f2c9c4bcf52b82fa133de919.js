ee6c527ce9c958ad32f8c3ca82ec22bae88613c0
Rafal Gozdz <rag@scalepoint.com>
2020-07-17 14:55:11 +0200

    Updates

---
+++ precache-manifest.b84c8447f2c9c4bcf52b82fa133de919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ed0d8fad48ca25deef67f3f2e1aedf",
+    "url": "/babbler/index.html"
+  },
+  {
+    "revision": "8e0ec946b3da69a47449",
+    "url": "/babbler/static/css/main.4abf37c0.chunk.css"
+  },
+  {
+    "revision": "71d2eea2a4e333af54f5",
+    "url": "/babbler/static/js/2.dea458b8.chunk.js"
+  },
+  {
+    "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
+    "url": "/babbler/static/js/2.dea458b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0ec946b3da69a47449",
+    "url": "/babbler/static/js/main.6490ea15.chunk.js"
+  },
+  {
+    "revision": "8b434f4d47be21b7333a",
+    "url": "/babbler/static/js/runtime-main.07fc5397.js"
+  },
+  {
+    "revision": "f5ceec7292d4140204fb468d10db668c",
+    "url": "/babbler/static/media/logo.f5ceec72.png"
+  }
+]);
