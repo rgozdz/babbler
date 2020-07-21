b5d2f69fe74fcd005d082331d21270025505f331
Rafal Gozdz <rag@scalepoint.com>
2020-07-21 15:43:55 +0200

    Babbler-4: footer with version

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -9,8 +8,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/rgozdz/">
+        aware4dev
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -42,26 +41,13 @@ export default function Footer() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
+    <footer className={classes.footer}>
+      <Container maxWidth="sm">
+        <Typography variant="body1">
+          Babbler ver.
         </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {"Pin a footer to the bottom of the viewport."}
-          {"The footer will move as the main element of the page grows."}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
+        <Copyright />
       </Container>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            My sticky footer can be found here.
-          </Typography>
-          <Copyright />
-        </Container>
-      </footer>
-    </div>
+    </footer>
   );
 }
